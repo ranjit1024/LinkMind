da4880313eb1b5f9a227d3a7584699075430d67f
ranjit1024 <ranjitdas2048@gmail.com>
2026-02-11 22:53:25 +0530

    somewaht

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex min-h-screen text-black bg-zinc-50">
      <header className="mt-7">
       <nav className=" flex justify-center items-center w-screen" >
-        <div className="border-[0.5px] p-3 border-zinc-200 flex rounded-xs">
+        <div className="border-[0.5px] p-3 border-zinc-200 flex rounded-xs border">
           <Image src={Logo} width={30} height={50} alt="Logo"/>
           <h1>LinkMind</h1>
         </div>
